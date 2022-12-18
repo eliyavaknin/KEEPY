f3f5f51ec5b542ebbf5c2a5accfbce55e43c9f8f
שיראל <SHIREL.5263@GMAIL.COM>
2022-12-18 23:47:01 +0200

    login signup mainactivity forgetpassword

--- app/src/main/java/com/example/keepy/MainActivity.java
+++ app/src/main/java/com/example/keepy/MainActivity.java
@@ -2,13 +2,42 @@ package com.example.keepy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button= findViewById(R.id.Buttonlogin);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openloginpage();
+            }
+
+        });
+        findViewById(R.id.ButtonSignUp).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {opensignuppage();}
+        }); ;
+
     }
+    public void openloginpage(){
+        Intent Intent =new Intent(MainActivity.this,LogIn.class);
+        startActivity(Intent);
+    }
+    public void opensignuppage(){
+        Intent Intent =new Intent(MainActivity.this,SignUp.class);
+        startActivity(Intent);
+    }
+
 }
