39bfd88f22d19bd79d39e838ba9496a7670eea6f
שיראל <SHIREL.5263@GMAIL.COM>
2022-12-28 17:25:59 +0200

    pagde user

--- app/src/main/java/com/example/keepy/SignUp.java
+++ app/src/main/java/com/example/keepy/SignUp.java
@@ -2,13 +2,30 @@ package com.example.keepy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class SignUp extends AppCompatActivity {
+
+    Spinner spinner;
+    String[] countries = {"Selest domain", "Clinte", "Service provider"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        spinner = findViewById(R.id.spinner);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_activated_1, countries);
+        spinner.setAdapter(adapter);
     }
 }
