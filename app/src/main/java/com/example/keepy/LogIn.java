2b29302858dc76bc85d43501d921f246f7d1c8cc
שיראל <SHIREL.5263@GMAIL.COM>
2023-01-03 23:16:05 +0200

    הוספת לוגו לאפליקציה הוספת splash sceen שינוי עמוד בית נותן שירות עיצוב עמוד בית לקוח מעבר על ידי לחיצה על textview לאקטיביטי אחר ובעמוד sign up הוספתי אופציה לבחור בין נותן למקבל שירות(אפשר לראות רק בהרצה)

--- app/src/main/java/com/example/keepy/LogIn.java
+++ app/src/main/java/com/example/keepy/LogIn.java
@@ -2,7 +2,10 @@ package com.example.keepy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LogIn extends AppCompatActivity {
 
@@ -10,5 +13,20 @@ public class LogIn extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+
+        View view = findViewById(R.id.editText1);
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openloginpage();
+            }
+
+        });
+
+    }
+
+    public void openloginpage() {
+        Intent Intent = new Intent(LogIn.this, ForgotPassword.class);
+        startActivity(Intent);
     }
 }
