e36b0ee33265f686f5d2b21bc6d118d3020f7a7f
שיראל <SHIREL.5263@GMAIL.COM>
2023-01-22 12:39:10 +0200

    profile

--- app/src/main/java/com/keepy/ForgotPassword.java
+++ app/src/main/java/com/keepy/ForgotPassword.java
@@ -22,11 +22,24 @@ public class ForgotPassword extends AppCompatActivity {
             }
 
         });
+        Button button= findViewById(R.id.ButtonReset);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openResetpage();
+            }
+
+        });
 
     }
 
     public void openloginpage() {
-        Intent Intent = new Intent(ForgotPassword.this, MainActivity.class);
+        Intent Intent = new Intent(ForgotPassword.this, LogIn.class);
+        startActivity(Intent);
+    }
+
+    public void openResetpage() {
+        Intent Intent = new Intent(ForgotPassword.this, EditProfile.class);
         startActivity(Intent);
     }
 }
